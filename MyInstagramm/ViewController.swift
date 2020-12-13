7a7a76d87fc5976b52c76b3c63b272d48da79e7e
Nikita <znv569@gmail.com>
2020-12-13 17:15:01 +0300

    Initial Commit

---
+++ MyInstagramm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyInstagramm
+//
+//  Created by Admin on 13.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
